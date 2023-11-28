598d516deeb6b7ee8479fde82a727a59c9946d50
nuridze <113994431+nuridze@users.noreply.github.com>
2023-11-28 17:49:51 +0100

    1

--- js/task-2.js
+++ js/task-2.js
@@ -1,6 +1,6 @@
 
 function formatMessage(message, maxLength){
-    if (message.length >= maxLength){
+    if (message.length > maxLength){
         return message.slice (0, maxLength)+ "...";
     } 
     else {return message};
